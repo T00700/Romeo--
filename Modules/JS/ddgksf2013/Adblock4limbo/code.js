53027ce4642fe60defb6cf57d1f90daf7c3258fe
ifflagged <ifflagged@users.noreply.github.com>
2025-10-15 13:12:55 +0000

    Synched Modules at 2025-10-15 21:12:54 (UTC+8)

--- Modules/JS/ddgksf2013/Adblock4limbo/code.js
+++ Modules/JS/ddgksf2013/Adblock4limbo/code.js
@@ -275,7 +275,7 @@ iframe {
 </head>
 
 <body>
-    <script async src="https://euob.youseasky.com/sxp/i/224f85302aa2b6ec30aac9a85da2cbf9.js" data-ch="AdsDeli - domain - landingpage" data-uvid="299762c58140a32eaf6d63c8d7f42cff636ce208" class="ct_clicktrue_80705" data-jsonp="onCheqResponse"></script>
+    <script async src="https://euob.youseasky.com/sxp/i/224f85302aa2b6ec30aac9a85da2cbf9.js" data-ch="AdsDeli - domain - landingpage" data-uvid="78de462e107cab06e64cc9496752b8559e4f03e6" class="ct_clicktrue_80705" data-jsonp="onCheqResponse"></script>
     <noscript>
         <iframe src="https://obseu.youseasky.com/ns/224f85302aa2b6ec30aac9a85da2cbf9.html?ch=AdsDeli%20-%20domain%20-%20landingpage"
                 width="0" height="0" style="display:none"></iframe>
@@ -307,10 +307,10 @@ var ls = function(xhr, token) {
         }
     }
     
-    xhr.open('GET', '/munin/a/l' + 's?t=68ef0391&token=' + encodeURI(token), true);
+    xhr.open('GET', '/munin/a/l' + 's?t=68ef9d36&token=' + encodeURI(token), true);
     xhr.send();
 };
-ls(new XMLHttpRequest(), '299762c58140a32eaf6d63c8d7f42cff636ce208');
+ls(new XMLHttpRequest(), '78de462e107cab06e64cc9496752b8559e4f03e6');
 if (typeof window.chronosFailed === 'function') { window.chronosFailed(); }
 </script>
 
@@ -347,7 +347,7 @@ if (typeof window.chronosFailed === 'function') { window.chronosFailed(); }
 </div>
 
 <div id="content">
-    <iframe id="iframe" frameBorder="0" src="https://yfdpco2.com/sk-park.php?pid=9PO15V947&dn=ra7.xyz&ua=Surge+Mac%2F2985&requrl=http%3A%2F%2Fra7.xyz%2Fz%2F.%2A%2Fcode.js"></iframe>
+    <iframe id="iframe" frameBorder="0" src="https://yfdnza.com/?dn=ra7.xyz&pid=9PO755G95"></iframe>
     </div>
 <script>
     (function () {
