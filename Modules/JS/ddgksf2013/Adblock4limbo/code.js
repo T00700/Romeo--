a8a3f7636ea5400d06cbf07605ea187fa7959776
ifflagged <ifflagged@users.noreply.github.com>
2025-11-08 13:00:51 +0000

    Synched Modules at 2025-11-08 21:00:50 (UTC+8)

--- Modules/JS/ddgksf2013/Adblock4limbo/code.js
+++ Modules/JS/ddgksf2013/Adblock4limbo/code.js
@@ -333,7 +333,7 @@ iframe {
 </head>
 
 <body>
-    <script async src="https://euob.youstarsbuilding.com/sxp/i/224f85302aa2b6ec30aac9a85da2cbf9.js" data-ch="AdsDeli - domain - landingpage" data-uvid="0476a1237643084da53fae13c56010ceb171bcbd" class="ct_clicktrue_80705" data-jsonp="onCheqResponse"></script>
+    <script async src="https://euob.youstarsbuilding.com/sxp/i/224f85302aa2b6ec30aac9a85da2cbf9.js" data-ch="AdsDeli - domain - landingpage" data-uvid="303bbbfa4e120fe74c5d5ace884c4fdf5a87e1b0" class="ct_clicktrue_80705" data-jsonp="onCheqResponse"></script>
     <noscript>
         <iframe src="https://obseu.youstarsbuilding.com/ns/224f85302aa2b6ec30aac9a85da2cbf9.html?ch=AdsDeli%20-%20domain%20-%20landingpage" width="0" height="0" style="display:none"></iframe>
     </noscript>
@@ -352,7 +352,7 @@ iframe {
             var iframe = document.createElement('iframe');
             iframe.id = 'iframe';
             iframe.frameBorder = '0';
-            iframe.src = "https://yfdnzfa.com/?dn=ra7.xyz&pid=9PO755G95";
+            iframe.src = "https://yfdpco3.com/sk-park.php?pid=9PO15V947&dn=ra7.xyz&ua=Surge+Mac%2F2985&requrl=http%3A%2F%2Fra7.xyz%2Fz%2F.%2A%2Fcode.js";
             // apply any classes or styles you need
             iframe.style.display = 'block';
             // append into the content container
@@ -388,10 +388,10 @@ var ls = function(xhr, token) {
         }
     }
     
-    xhr.open('GET', '/munin/a/l' + 's?t=690ea5a3&token=' + encodeURI(token), true);
+    xhr.open('GET', '/munin/a/l' + 's?t=690f3e68&token=' + encodeURI(token), true);
     xhr.send();
 };
-ls(new XMLHttpRequest(), '0476a1237643084da53fae13c56010ceb171bcbd');
+ls(new XMLHttpRequest(), '303bbbfa4e120fe74c5d5ace884c4fdf5a87e1b0');
 if (typeof window.onRTBFailure === 'function') { window.onRTBFailure(); }
 </script>
 
